bea7a743105ec4987e5dc71493d4cdc890270538
Sondos Amr <sondos.amr444@gmail.com>
2025-02-08 23:05:26 +0200

    Add basic form layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ function App() {
   return (
     <div className="App">
       <h1>hello world!</h1>
+      <hr />
+      <LenoForm />
+    </div>
+  );
+}
+
+function LenoForm() {
+  return (
+    <div className="container-form">
+      <form className="form">
+        <label>Name : </label>
+        <input type="text" placeholder="Name" />
+        <label>Phone Number : </label>
+        <input type="phone" placeholder="Phone" />
+        <label>Age :</label>
+        <input type="number" />
+        <label>Are You an employee ?</label>
+        <input type="checkBox" />
+        <label>Salary</label>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
