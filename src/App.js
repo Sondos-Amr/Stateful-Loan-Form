c19db47890fab91c74b2a9f7909460b3b3593f4e
Sondos Amr <sondos.amr444@gmail.com>
2025-02-16 22:48:59 +0200

    Refactored form state management by creating a reusable function to handle input changes. This eliminates redundancy, improves code maintainability, and makes it easier to manage form data dynamically.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import "./App.css";
-import Popup from "./Popup";
+// import Popup from "./Popup";
 function App() {
   return (
     <div className="App flex">
@@ -7,31 +8,74 @@ function App() {
         <h1> Repuestung a Loan </h1>
         <hr />
         <LenoForm />
-        <Popup />
+        {/* <Popup /> */}
       </div>
     </div>
   );
 }
 
 function LenoForm() {
+  const [formInput, setFormInput] = useState({
+    name: "",
+    tel: "",
+    age: "",
+    isEmployee: false,
+    salary: "",
+  });
+  function handleInputChange(e) {
+    const { name, checked, type, value } = e.target;
+    setFormInput((prev) => ({
+      ...prev,
+      [name]: type === "checked" ? checked : value,
+    }));
+  }
+  const [isDisabled, setDisable] = useState(true);
   return (
     <div className="container-form flex">
       <form className="form flex">
         <label>Name : </label>
-        <input type="text" />
+        <input
+          name="name"
+          type="text"
+          value={formInput.name}
+          onChange={handleInputChange}
+        />
         <label>Phone Number : </label>
-        <input type="tel" />
+        <input
+          name="tel"
+          type="tel"
+          value={formInput.tel}
+          onChange={handleInputChange}
+        />
         <label>Age :</label>
-        <input type="text" />
+        <input
+          name="age"
+          type="text"
+          value={formInput.age}
+          onChange={handleInputChange}
+        />
         <label>Are You an employee ?</label>
-        <input type="checkBox" />
+        <input
+          name="isEmployee"
+          type="checkbox"
+          checked={formInput.isEmployee}
+          onChange={(e) =>
+            setFormInput({ ...formInput, isEmployee: e.target.checked })
+          }
+        />
         <label>Salary</label>
-        <select>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
+        <select
+          name="salary"
+          value={formInput.salary}
+          onChange={handleInputChange}
+        >
+          <option>$1000</option>
+          <option>$2000</option>
+          <option>$3000</option>
         </select>
-        <button className="form-btn">Submit</button>
+        <button className="form-btn" disabled={(e) => setDisable(!isDisabled)}>
+          Submit
+        </button>
       </form>
     </div>
   );
